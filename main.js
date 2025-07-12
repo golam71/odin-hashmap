3a0087bee4beb6c757934c5236e7038b32fd2e72
golam71 <74001687+golam71@users.noreply.github.com>
2025-07-12 23:48:23 +0600

    :fire: Remove LinkedList implementation and refactor Hashmap to use Node class

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-import { LinkedList } from "./linkedList.js";
+class Node {
+  constructor(key, value, nextNode = null) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
 
 class Hashmap {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.buckets = [, , , , , , , , , , , , , , , ,];
+  }
+  length() {
+    return this.buckets.length;
+  }
+  clear() {
+    this.buckets = [];
+  }
+  get(key) {
+    let hashKey = this.hash(key);
+    let bucketIndex = hashKey % this.length();
+
+    if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    // console.log(this.buckets[bucketIndex]);
+    let currentNode = this.buckets[bucketIndex];
+    while (currentNode != null) {
+      if (currentNode.key == key) {
+        return currentNode.value;
+      }
+      currentNode = currentNode.nextNode;
+    }
+    return null;
+  }
+
   hash(key) {
     let hashCode = 0;
 
@@ -15,7 +50,29 @@ class Hashmap {
 
     return hashCode;
   }
+  set(key, value) {
+    let hashKey = this.hash(key);
+    let bucketIndex = hashKey % this.length();
+    let node = new Node(key, value);
+
+    if (this.buckets[bucketIndex] == undefined) {
+      this.buckets[bucketIndex] = node;
+    } else {
+      let currentNode = this.buckets[bucketIndex];
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = node;
+    }
+  }
 }
 
 let x = new Hashmap();
-console.log(x.hash("test"));
+// console.log(x.length());
+x.set("hello", "world");
+x.set("foo", "bar");
+x.set("baz", "qux");
+x.set("test", "value");
+
+// console.log(x.buckets);
+console.log(x.get("key49"));
